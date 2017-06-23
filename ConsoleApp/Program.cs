8113c47dc96a938cc6dc79101140cfd5e2cf48ea
Shi Hao Wang <st890409@hotmail.com>
2017-06-23 16:45:16 +0800

    主控台應用程式模擬檔案存取進度條
    使用 .NET Core 主控台應用程式
    模擬檔案存取進度條：提供開始，處理中，處理完畢的事件

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using HttpSimulator;
+using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static int _positionTop;
+        static int _positionLeft;
+        static int _presentLeft;
+        static int _presentTotal = 100;
+
+
+        static void Main(string[] args)
+        {
+            ImageDownloader _downloader;
+
+            _downloader = new ImageDownloader();
+            _downloader.DowloadValueChange += _downloader_DowloadValueChange;
+            _downloader.DownloadStarting += _downloader_DownloadStarting;
+            _downloader.DownloadFinish += _downloader_DownloadFinish;
+            _downloader.Download(@"http://url/");
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+
+
+        //檔案下載開始事件
+        private static void _downloader_DownloadStarting(object sender, ImageDownloadStartEventArgs e)
+        {
+            Console.WriteLine("start <\"{0}\"> downloading", e.Url);
+            Console.Write("[");
+
+            _positionTop = Console.CursorTop;
+            _positionLeft = Console.CursorLeft;
+
+            for (int i = 0; i <= 25; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("] ");
+
+            _presentLeft = Console.CursorLeft;
+
+            Console.Write("   /100 (%)");
+        }
+
+        //下載作業完成事件
+        private static void _downloader_DownloadFinish(object sender, ImageDownloadFinishEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("download finished");
+        }
+
+        //下載進度變更事件
+        private static void _downloader_DowloadValueChange(object sender, ImageDownloadChangeEventArgs e)
+        {
+            var _currentValue = e.CurrentValue;
+            var _tick = Convert.ToInt32(_currentValue * 25 / _presentTotal);
+
+            Console.SetCursorPosition(_positionLeft + _tick, _positionTop);
+            Console.Write("-");
+
+            Console.SetCursorPosition(_presentLeft, _positionTop);
+            Console.Write(_currentValue.ToString().PadLeft(3,' '));
+        }
+    }
+}
---
+++ HttpSimulator/ImageDownloader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+
+namespace HttpSimulator
+{
+    #region 事件狀態物件
+    
+    public class ImageDownloadStartEventArgs
+    {
+        private string _url;
+
+        public ImageDownloadStartEventArgs(string url)
+        {
+            this._url = url;
+        }
+
+        public string Url
+        {
+            get
+            {
+                return this._url;
+            }
+        }
+    }
+
+    public class ImageDownloadFinishEventArgs { }
+
+    public class ImageDownloadChangeEventArgs
+    {
+        private int _currentValue;
+
+        public ImageDownloadChangeEventArgs(int currentValue)
+        {
+            this._currentValue = currentValue;
+        }
+
+        public int CurrentValue
+        {
+            get
+            {
+                return this._currentValue;
+            }
+        }
+    }
+
+    #endregion
+
+
+    /// <summary>
+    /// 模擬下載物件
+    /// </summary>
+    public class ImageDownloader
+    {
+        const int sleepTick = 250;
+        const int presentTotal = 100;
+
+        //註冊事件：方法-1
+        //public delegate void DownloadChangeEventHandler(object sender, ImageDownloadChangeEventArgs e);
+        //public event DownloadChangeEventHandler DowloadValueChange;
+
+        //public delegate void DownloadStartEventHandler(object sender, ImageDownloadStartEventArgs e);
+        //public event DownloadStartEventHandler DownloadStarting;
+
+        //public delegate void DownloadFinishEventHandler(object sender, ImageDownloadFinishEventArgs e);
+        //public event DownloadFinishEventHandler DownloadFinish;
+
+        //註冊事件：方法-2
+        public event EventHandler<ImageDownloadChangeEventArgs> DowloadValueChange;
+        public event EventHandler<ImageDownloadStartEventArgs> DownloadStarting;
+        public event EventHandler<ImageDownloadFinishEventArgs> DownloadFinish;
+        
+
+
+        public void Download(string url)
+        {
+            int _currentValue = 0;
+
+            this.DownloadStarting(this, new ImageDownloadStartEventArgs(url));
+
+            while(_currentValue < presentTotal)
+            {
+                _currentValue = _currentValue + 1;
+
+                if(DowloadValueChange != null)
+                    this.DowloadValueChange(this, new ImageDownloadChangeEventArgs(_currentValue));
+
+                Thread.Sleep(sleepTick);
+            }
+
+            this.DownloadFinish(this, new ImageDownloadFinishEventArgs());
+        }
+
+    }
+}
